33cc3b48a64e1cebd7a80e314bc13f5f3fb2dc22
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2025-09-03 12:14:38 +0530

    Create Binary_Search_Recursive.cpp

---
+++ Binary_Search_Recursive.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int binarySearch(int arr[], int left, int right, int x) {
+    if (right >= left) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, left, mid - 1, x);
+        return binarySearch(arr, mid + 1, right, x);
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int x;
+    cout << "Enter the element to search: ";
+    cin >> x;
+    int result = binarySearch(arr, 0, n - 1, x);
+    if (result != -1) {
+        cout << "Element " << x << " is present at index " << result << endl;
+    } else {
+        cout << "Element " << x << " is not present in the array." << endl;
+    }
+    return 0;
+}
